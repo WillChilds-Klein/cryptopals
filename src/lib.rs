739c3f31dae7a9604d2932ccde37c732194d0290
Will Childs-Klein <childw@amazon.com>
2022-01-10 16:57:47 -0500

    Set 1, Challenge 4-5

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,6 @@ fn to_base64(input: Vec<u8>) -> String {
     base64::encode(input)
 }
 
-// Challenge 1.2
 fn xor(one: &Vec<u8>, two: &Vec<u8>) -> Vec<u8> {
     assert_eq!(one.len(), two.len());
     let mut output = Vec::new();
@@ -31,15 +30,23 @@ fn xor(one: &Vec<u8>, two: &Vec<u8>) -> Vec<u8> {
     output
 }
 
-// Challenge 1.3
-fn find_single_byte_xor_key(input: Vec<u8>) -> u8 {
-    let mut lowest = (0u8, f32::MAX);
+fn find_single_byte_xor_key(input: &Vec<u8>) -> u8 {
+    let mut lowest = (0u8, f32::MAX, 0f32); // value, score, lower-case ratio
     for c in 0..u8::MAX {
         match String::from_utf8(xor(&input, &vec![c; input.len()])) {
             Ok(s) => {
                 let score = english_text_score(s.as_str());
                 if score < lowest.1 {
-                    lowest = (c, score);
+                    // NOTE: there will always be 2 keys that result in the same score (with a value
+                    //       difference of 32), so assume real sentences tend to have higher ratio
+                    //       of lower-case letters. use this heuristic to break the tie.
+                    let lower_case_count = s.chars()
+                        .filter(char::is_ascii_lowercase)
+                        .count();
+                    let lower_case_ratio = lower_case_count as f32 /  s.len() as f32;
+                    if lower_case_ratio > lowest.2 {
+                        lowest = (c, score, lower_case_ratio);
+                    }
                 }
             },
             Err(_) => {
@@ -49,7 +56,6 @@ fn find_single_byte_xor_key(input: Vec<u8>) -> u8 {
     }
     lowest.0
 }
-
 
 /// Returns a score indicating "englishness" of a string
 ///
@@ -104,7 +110,7 @@ fn english_text_score(input: &str) -> f32 {
         }
     }
     // calculate the score, building from 0.0
-    let mut score: f32 = 0.0;
+    let mut score = 0f32;
     for (c, _) in reference_frequencies.iter() {
         let reference_frequency = *reference_frequencies.get(&c).unwrap();
         let count = *char_counts.get(&c).unwrap();
@@ -115,18 +121,28 @@ fn english_text_score(input: &str) -> f32 {
     score
 }
 
+fn repeating_key_xor(message: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
+    let mut full_key = Vec::new();
+    for (ii, _) in message.iter().enumerate() {
+        full_key.push(key[ii % key.len()]);
+    }
+    xor(&message, &full_key)
+}
+
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use crate::*;
 
-    #[test]
+    #[test] // Challenge 1.1
     fn test_base16_to_base64() {
         let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
         assert_eq!(output, to_base64(from_base16(input)));
     }
 
-    #[test]
+    #[test] // Challenge 1.2
     fn test_xor() {
         let one = "1c0111001f010100061a024b53535009181c";
         let two = "686974207468652062756c6c277320657965";
@@ -134,9 +150,54 @@ mod tests {
         assert_eq!(output, to_base16(xor(&from_base16(one), &from_base16(two))));
     }
 
-    #[test]
+    #[test] // Challenge 1.3
     fn test_find_single_char_xor_decrypt() {
         let ciphertext = from_base16("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
-        assert_eq!('X', find_single_byte_xor_key(ciphertext) as char);
+        let key = 'X' as u8;
+        let plaintext = String::from_utf8(xor(&ciphertext, &vec![key; ciphertext.len()])).unwrap();
+        assert_eq!(key, find_single_byte_xor_key(&ciphertext));
+        assert_eq!("Cooking MC's like a pound of bacon", plaintext);
+    }
+
+    #[test] // Challenge 1.4
+    fn test_find_single_char_xor_decrypt_from_file() {
+        let ciphertexts: Vec<Vec<u8>> = fs::read_to_string("tst/data/01_04.txt")
+            .expect("Cannot read input data")
+            .lines()
+            .map(str::trim)
+            .map(from_base16)
+            .collect();
+        let mut lowest = (f32::MAX, 0u8, Vec::new());   // score, key, ciphertext
+        for ciphertext in ciphertexts {
+            let key = find_single_byte_xor_key(&ciphertext);
+            let plaintext = xor(&ciphertext, &vec![key; ciphertext.len()]);
+            let score = match String::from_utf8(plaintext) {
+                Ok(s) => {
+                    english_text_score(&s)
+                },
+                Err(_) => {
+                    continue;
+                }
+            };
+            if score < lowest.0 {
+                lowest = (score, key, Vec::from(ciphertext));
+            }
+        }
+        let plaintext = String::from_utf8(
+            xor(&lowest.2, &vec![lowest.1; lowest.2.len()])
+        ).unwrap();
+        assert_eq!(53u8, lowest.1);
+        assert_eq!("Now that the party is jumping\n", plaintext);
+    }
+
+    #[test] // Challenge 1.5
+    fn test_repeating_key_xor_encrypt_decrypt() {
+        let expected_ciphertext = from_base16("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
+        let plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes().to_vec();
+        let key = "ICE".as_bytes().to_vec();
+        let actual_ciphertext = repeating_key_xor(&plaintext, &key);
+        assert_eq!(expected_ciphertext, actual_ciphertext);
+        let decrypted_plaintext = repeating_key_xor(&actual_ciphertext, &key);
+        assert_eq!(plaintext, decrypted_plaintext);
     }
 }
